9657ad4a88ca68109ce7a07bf0264e720217b82a
Vladimir <vtischenko@meta.ua>
2018-07-26 09:09:09 +0300

    Initial

---
+++ src/main/java/moneyManager/Main.java
@@ -0,0 +1,10 @@
+package moneyManager;
+
+/**
+ * Created by Vladimir on 26.07.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Hello Topjava Enterprise!");
+    }
+}
