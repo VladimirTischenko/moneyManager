a80e4c13f5ade3708866d4c309aec8f3a17395ee
Vladimir <vtischenko@meta.ua>
2018-07-31 18:50:54 +0300

    1 4 add servlet api

---
+++ src/main/java/moneyManager/web/UserServlet.java
@@ -0,0 +1,16 @@
+package moneyManager.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Vladimir on 31.07.2018.
+ */
+public class UserServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
+    }
+}
