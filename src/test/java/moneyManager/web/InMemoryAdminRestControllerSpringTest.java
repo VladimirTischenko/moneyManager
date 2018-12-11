d6d91de8b16cb910360ecdaf31f7074dd8c9fdd9
Vladimir <vtischenko@meta.ua>
2018-12-11 20:37:51 +0300

    3 08 add spring test

--- src/test/java/moneyManager/web/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/moneyManager/web/InMemoryAdminRestControllerSpringTest.java
@@ -5,36 +5,30 @@ import moneyManager.model.User;
 import moneyManager.repository.UserRepository;
 import moneyManager.util.exception.NotFoundException;
 import moneyManager.web.user.AdminRestController;
-import org.junit.*;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 import static moneyManager.UserTestData.ADMIN;
 import static moneyManager.UserTestData.USER;
 
-public class InMemoryAdminRestControllerTest {
-    private static ConfigurableApplicationContext appCtx;
-    private static AdminRestController controller;
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class InMemoryAdminRestControllerSpringTest {
+    @Autowired
+    private AdminRestController controller;
 
-    @BeforeClass
-    public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
-        controller = appCtx.getBean(AdminRestController.class);
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        appCtx.close();
-    }
+    @Autowired
+    private UserRepository repository;
 
     @Before
     public void setUp() {
-        // Re-initialize
-        UserRepository repository = appCtx.getBean(UserRepository.class);
         repository.getAll().forEach(u -> repository.delete(u.getId()));
         repository.save(USER);
         repository.save(ADMIN);
