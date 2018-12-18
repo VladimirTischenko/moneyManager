0480f3d5b55c1cd155a39feff6eaf0a941f21d75
Vladimir <vtischenko@meta.ua>
2018-12-18 09:06:03 +0300

    4 7 add named query and transaction

--- src/main/java/moneyManager/repository/jpa/JpaUserRepositoryImpl.java
+++ src/main/java/moneyManager/repository/jpa/JpaUserRepositoryImpl.java
@@ -2,14 +2,16 @@ package moneyManager.repository.jpa;
 
 import moneyManager.model.User;
 import moneyManager.repository.UserRepository;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaUserRepositoryImpl implements UserRepository {
 /*
     @Autowired
@@ -24,6 +26,7 @@ public class JpaUserRepositoryImpl implements UserRepository {
     private EntityManager em;
 
     @Override
+    @Transactional
     public User save(User user) {
         if (user.isNew()) {
             em.persist(user);
@@ -39,22 +42,26 @@ public class JpaUserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
-
-/*      User ref = em.getReference(User.class, id);
+/*
+        User ref = em.getReference(User.class, id);
         em.remove(ref);
-*/
-        Query query = em.createQuery("DELETE FROM User u WHERE u.id=:id");
+
+        Query<User> query = em.createQuery("DELETE FROM User u WHERE u.id=:id");
         return query.setParameter("id", id).executeUpdate() != 0;
+*/
+        return em.createNamedQuery(User.DELETE).setParameter("id", id).executeUpdate() != 0;
     }
 
     @Override
     public User getByEmail(String email) {
-        return null;
+        List<User> users = em.createNamedQuery(User.BY_EMAIL, User.class).setParameter(1, email).getResultList();
+        return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return em.createNamedQuery(User.ALL_SORTED, User.class).getResultList();
     }
 }
