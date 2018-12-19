d44a871423bb7ebbf98f337256c5ca85ce42712e
Vladimir <vtischenko@meta.ua>
2018-12-19 08:59:05 +0300

    5 1 HW4

--- src/main/java/moneyManager/model/Cost.java
+++ src/main/java/moneyManager/model/Cost.java
@@ -1,7 +1,10 @@
 package moneyManager.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -9,14 +12,36 @@ import java.time.LocalTime;
 /**
  * Created by Vladimir on 26.07.2018.
  */
+@SuppressWarnings("JpaQlInspection")
+@NamedQueries({
+        @NamedQuery(name = Cost.ALL_SORTED, query = "SELECT c FROM Cost c WHERE c.user.id=:userId ORDER BY c.dateTime DESC"),
+        @NamedQuery(name = Cost.DELETE, query = "DELETE FROM Cost c WHERE c.id=:id AND c.user.id=:userId"),
+        @NamedQuery(name = Cost.GET_BETWEEN, query = "SELECT c FROM Cost c " +
+                "WHERE c.user.id=:userId AND c.dateTime BETWEEN :startDate AND :endDate ORDER BY c.dateTime DESC"),
+//        @NamedQuery(name = Cost.UPDATE, query = "UPDATE Cost c SET c.dateTime = :datetime, c.price= :price," +
+//                "c.description=:desc where c.id=:id and c.user.id=:userId")
+})
+@Entity
+@Table(name = "costs", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "costs_unique_user_datetime_idx")})
 public class Cost extends BaseEntity {
+    public static final String ALL_SORTED = "Cost.getAll";
+    public static final String DELETE = "Cost.delete";
+    public static final String GET_BETWEEN = "Cost.getBetween";
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
     private String description;
 
+    @Column(name = "price", nullable = false)
+    @Range(min = 10, max = 5000)
     private int price;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Cost() {
--- src/main/java/moneyManager/repository/jpa/JpaCostRepositoryImpl.java
+++ src/main/java/moneyManager/repository/jpa/JpaCostRepositoryImpl.java
@@ -1,36 +1,64 @@
 package moneyManager.repository.jpa;
 
 import moneyManager.model.Cost;
+import moneyManager.model.User;
 import moneyManager.repository.CostRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaCostRepositoryImpl implements CostRepository {
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public Cost save(Cost cost, int userId) {
-        return null;
+        if (!cost.isNew() && get(cost.getId(), userId) == null) {
+            return null;
+        }
+        cost.setUser(em.getReference(User.class, userId));
+        if (cost.isNew()) {
+            em.persist(cost);
+            return cost;
+        } else {
+            return em.merge(cost);
+        }
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createNamedQuery(Cost.DELETE)
+                .setParameter("id", id)
+                .setParameter("userId", userId)
+                .executeUpdate() != 0;
     }
 
     @Override
     public Cost get(int id, int userId) {
-        return null;
+        Cost cost = em.find(Cost.class, id);
+        return cost != null && cost.getUser().getId() == userId ? cost : null;
     }
 
     @Override
     public List<Cost> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(Cost.ALL_SORTED, Cost.class)
+                .setParameter("userId", userId)
+                .getResultList();
     }
 
     @Override
     public List<Cost> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return em.createNamedQuery(Cost.GET_BETWEEN, Cost.class)
+                .setParameter("userId", userId)
+                .setParameter("startDate", startDate)
+                .setParameter("endDate", endDate).getResultList();
     }
 }
