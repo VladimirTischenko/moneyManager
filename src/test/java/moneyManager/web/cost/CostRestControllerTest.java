5b00856bf178696071c88ec43595cd7c4af4fb95
VladimirTischenko <vtischenko@meta.ua>
2019-04-29 16:41:25 +0300

    10 02 HW9 test

--- src/test/java/moneyManager/web/cost/CostRestControllerTest.java
+++ src/test/java/moneyManager/web/cost/CostRestControllerTest.java
@@ -13,11 +13,13 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.Arrays;
 
 import static moneyManager.CostTestData.*;
+import static moneyManager.TestUtil.userHttpBasic;
+import static moneyManager.UserTestData.ADMIN;
+import static moneyManager.UserTestData.ADMIN_ID;
 import static moneyManager.UserTestData.USER;
 import static moneyManager.model.BaseEntity.START_SEQ;
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,16 +32,24 @@ public class CostRestControllerTest extends AbstractControllerTest {
 
     @Test
     public void testGet() throws Exception {
-        mockMvc.perform(get(REST_URL + COST1_ID))
+        mockMvc.perform(get(REST_URL + ADMIN_COST_ID)
+                .with(userHttpBasic(ADMIN)))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(MATCHER.contentMatcher(COST1));
+                .andExpect(MATCHER.contentMatcher(ADMIN_COST1));
+    }
+
+    @Test
+    public void testGetUnauth() throws Exception {
+        mockMvc.perform(get(REST_URL + COST1_ID))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
     public void testDelete() throws Exception {
-        mockMvc.perform(delete(REST_URL + COST1_ID).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(delete(REST_URL + COST1_ID)
+                .with(userHttpBasic(USER)))
                 .andExpect(status().isOk());
         MATCHER.assertCollectionEquals(Arrays.asList(COST6, COST5, COST4, COST3, COST2), service.getAll(START_SEQ));
     }
@@ -48,8 +58,10 @@ public class CostRestControllerTest extends AbstractControllerTest {
     public void testUpdate() throws Exception {
         Cost updated = getUpdated();
 
-        mockMvc.perform(put(REST_URL + COST1_ID).contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(updated)))
+        mockMvc.perform(put(REST_URL + COST1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated))
+                .with(userHttpBasic(USER)))
                 .andExpect(status().isOk());
 
         assertEquals(updated, service.get(COST1_ID, START_SEQ));
@@ -60,18 +72,20 @@ public class CostRestControllerTest extends AbstractControllerTest {
         Cost created = getCreated();
         ResultActions action = mockMvc.perform(post(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(created)));
+                .content(JsonUtil.writeValue(created))
+                .with(userHttpBasic(ADMIN)));
 
         Cost returned = MATCHER.fromJsonAction(action);
         created.setId(returned.getId());
 
         MATCHER.assertEquals(created, returned);
-        MATCHER.assertCollectionEquals(Arrays.asList(created, COST6, COST5, COST4, COST3, COST2, COST1), service.getAll(START_SEQ));
+        MATCHER.assertCollectionEquals(Arrays.asList(created, ADMIN_COST2, ADMIN_COST1), service.getAll(ADMIN_ID));
     }
 
     @Test
     public void testGetAll() throws Exception {
-        mockMvc.perform(get(REST_URL).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get(REST_URL)
+                .with(userHttpBasic(USER)))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
@@ -80,7 +94,8 @@ public class CostRestControllerTest extends AbstractControllerTest {
 
     @Test
     public void testGetBetween() throws Exception {
-        mockMvc.perform(get(REST_URL + "between?startDateTime=2018-07-21T07:00&endDateTime=2018-07-22T12:00:00"))
+        mockMvc.perform(get(REST_URL + "between?startDateTime=2018-07-21T07:00&endDateTime=2018-07-22T12:00:00")
+                .with(userHttpBasic(USER)))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(MATCHER_WITH_EXCEED.contentListMatcher(
@@ -92,7 +107,8 @@ public class CostRestControllerTest extends AbstractControllerTest {
     public void testFilter() throws Exception {
         mockMvc.perform(get(REST_URL + "filter")
                 .param("startDate", "2018-07-21").param("startTime", "07:00")
-                .param("endDate", "2018-07-22").param("endTime", "12:00"))
+                .param("endDate", "2018-07-22").param("endTime", "12:00")
+                .with(userHttpBasic(USER)))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(MATCHER_WITH_EXCEED.contentListMatcher(
@@ -100,10 +116,10 @@ public class CostRestControllerTest extends AbstractControllerTest {
                         CostsUtil.createWithExceed(COST1, false)));
     }
 
-
     @Test
     public void testFilterAll() throws Exception {
-        mockMvc.perform(get(REST_URL + "filter?startDate=&endTime="))
+        mockMvc.perform(get(REST_URL + "filter?startDate=&endTime=")
+                .with(userHttpBasic(USER)))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(MATCHER_WITH_EXCEED.contentListMatcher(
